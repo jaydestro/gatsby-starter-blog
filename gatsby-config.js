16dc3e50a574756ee3bf21a18d68a1c25209f2aa
Jay Gordon <jaydestro@gmail.com>
2021-05-21 10:13:25 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Jay Gordon`,
+      summary: `Friend to dogs.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `jaydestro`,
     },
   },
   plugins: [
